1112562162808012eb11b858b2e4660bade459e1
Gabriel Duarte <gabriellagoa10@yahoo.com.br>
2017-05-19 19:49:04 -0300

    Welcome email for new user

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,28 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const nodemailer = require('nodemailer');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport(
+    'smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com');
+
+
+exports.welcomeFunction = 
+		functions.database.ref('/user/{pushId}/email').onWrite(event =>{
+
+			// Looking just for a new register
+			if(!event.data.val() || event.data.previous.val()){
+				return;
+			}
+
+			const email = event.data.val();
+			const mailOptions = {
+				    from: '"Chat App." <duagabriel@gmail.com>',
+				    to: email
+				};
+			mailOptions.subject = 'Thanks and Welcome!';
+    		mailOptions.text = 'Thanks you for register in my chat app.';
+    		return mailTransport.sendMail(mailOptions);
+		});
+
+
